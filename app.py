51019eb1f545389c1bd0b781eeb9be0f7d8b179c
Cleber <clebmachado.em@gmail.com>
2021-12-19 16:31:21 -0300

    Feito um exemplo de consumo da API

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from collections import namedtuple
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 import json
+
+
+from flask.templating import render_template
 from functions.tesouro_direto import calcular_tesouro
 
 app = Flask(__name__)
 
-@app.route("/<aportInicial>/<aporteMensal>", methods=["GET", 'POST'])
-def home(aportInicial, aporteMensal):
-    # dados_user = request.form["user_parametros"]
-    # dados_user = json.loads(data_selecionadas)
+@app.route("/tesouros", methods=["POST"])
+def tesouro():
+    req = request.get_json()
     Investimento = namedtuple('Investimento', ['bruto', 'total', 'imposto', "b3", "liquido"])
-    fundo = calcular_tesouro(float(aportInicial), float(aporteMensal), "TESOURO PREFIXADO 2026")
+    fundo = calcular_tesouro(float(req["aporteInicial"]), float(req["aporteMensal"]), "TESOURO PREFIXADO 2026")
     investimento = Investimento(fundo[0], fundo[1], fundo[2],fundo[3],fundo[4])
-    return json.dumps(investimento._asdict())
+    res = make_response(jsonify(investimento._asdict()), 200)
+    return res
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 app.run(debug=True)
