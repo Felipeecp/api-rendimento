a47f34c1808612be37526c30db2c9cf4d2f636ad
Felipeecp <felipe.engcp@gmail.com>
2021-12-20 12:52:11 -0300

    teste request

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ app = Flask(__name__)
 
 @app.route("/tesouros", methods=["POST"])
 def tesouro():
-    req = request.get_json()
-    print(req)
+    # req = request.get_json()
+    # print(req)
     Investimento = namedtuple('Investimento', ['bruto', 'total', 'imposto', "b3", "liquido"])
-    fundo = calcular_tesouro(float(req["aporteInicial"]), float(req["aporteMensal"]), "TESOURO PREFIXADO 2026")
-    investimento = Investimento(fundo[0], fundo[1], fundo[2],fundo[3],fundo[4])
+    # fundo = calcular_tesouro(float(req["aporteInicial"]), float(req["aporteMensal"]), "TESOURO PREFIXADO 2026")
+    investimento = Investimento(1500, 2000, 500,300,100)
+    # res = make_response(jsonify(investimento._asdict()), 200)
     res = make_response(jsonify(investimento._asdict()), 200)
     return res
 
