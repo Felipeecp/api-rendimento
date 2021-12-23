e371d529875701275f9955206da6b41598d61dd2
Felipeecp <felipe.engcp@gmail.com>
2021-12-23 09:38:08 -0300

    obtendo dados online

--- functions/tesouro_direto.py
+++ functions/tesouro_direto.py
@@ -3,33 +3,32 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 def getTitulos():
-    
-    titulos = pd.read_csv("functions/data/titulos.csv",sep=';')
-    titulos = titulos[titulos['Compra'] != '-']
+    url = 'https://apiapex.tesouro.gov.br/aria/v1/sistd/custom/ultimaRentabilidadeCSV'
+    titulos = pd.read_csv(url, sep=";", encoding="latin1")
+    titulos[['Últ. 30 dias','Mêsanterior','No ano','12 meses','Compra','Venda']] = titulos[['Últ. 30 dias','Mêsanterior','No ano','12 meses','Compra','Venda']].apply(lambda x: x.str.replace(',','.'))
 
-    def arrumar(nomeTitulo):
-        nome_lista = nomeTitulo.split()
-        return " ".join(nome_lista)
-
-    titulos["Título"] = titulos["Título"].apply(arrumar)
+    titulos.query("Compra != '-'",inplace=True)
+    titulos['Vencimento'] = pd.to_datetime(titulos['Vencimento'],format="%d/%m/%Y")
+    titulos.reset_index(drop=True,inplace=True)
+    for i in range(len(titulos)):
+        titulos['Títulos'][i] = f"{titulos['Títulos'][i]} {titulos['Vencimento'][i].year}"
 
     return titulos
 
 
-
-def calcular_tesouro(inicial, aporte_mensal, nome_do_titulo, dataset=getTitulos()):
-    df = dataset[dataset["Título"] == nome_do_titulo]
+def calcular_tesouro(inicial, aporte_mensal, nome_do_titulo, dataset):
+    df = dataset[dataset["Títulos"] == nome_do_titulo]
 
     # Pegando a quantidade de meses
     startdt=pd.to_datetime(datetime.now().date())
-    enddt = pd.to_datetime(datetime.strptime(df["Vencimento"].values[0], '%d/%m/%Y'))
+    enddt = df["Vencimento"].iloc[0]
     len(pd.date_range(start=startdt,end=enddt,freq='M'))
 
     tempo = int(len(pd.date_range(start=startdt,end=enddt,freq='M')) - 1)
 
     # Calculo da taxa equivalente
     temp_t_equivalente = 1
-    taxa_compra = df[df["Título"] == nome_do_titulo]['Compra']
+    taxa_compra = df[df["Títulos"] == nome_do_titulo]['Compra']
     taxa_juros = float(taxa_compra)/100
     temp_t_atual = 12
     taxa_equivalente = (1 + taxa_juros) ** (temp_t_equivalente / temp_t_atual) - 1
